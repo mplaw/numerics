8b7153ddab2e4a9bdf0ec167059fab2d543628a7
mplaw <m.p.law93@gmail.com>
2016-02-26 11:56:18 +0000

    Update bound.h
    Changes:
    - Replaced std library overloads with the ability to implicitly cast to type T (specified by the template).
    - Added flag: MANUAL_CHECK, which removes bounds checking from operation, so that the user can manually check later. As a corollary of this, I've moved the check function into the public namespace.
    - Used copy-swap idiom for assignment operator for T type, which will be of benefit when T is not a primitive.
    - Added noexcept where possible, which guarantees the function will not throw an exception. noexcept is only applied if the default constructor of T is also non-throwing.
    - Changed swap to a friend function, which will allow the STL to use it instead of std::swap for STL containers.

--- bound.h
+++ bound.h
@@ -5,14 +5,22 @@
 		keyord from the "void check()" function. (virtual slows things down a little and 
 		prevents compiler inlining).
 
-		- finish type casts
+		- add no except where appropriate and possible
 
-		- add compiler optimisations, turn off excepptions
-
-		- std library overloads (or maybe let the user cast)
+		- consdier replacing min, val and max with an array of type T, so that swap can just swap the pointers to the start element and thus be more efficient.
+		note: this would likely increase look-up time of 2nd and 3rd elements, as it would require computing their position in the array.
 */
 
 #include <iostream>
+
+#define MANUAL_CHECKING	0		// Turns the check function off, so the bound object's value is no longer bounded automatically. You can manually enforce bounds by calling check().
+								// This will allow you to chain-together lots of operations, and then check at the end (as opposed to check being called after each operation).
+
+#if MANUAL_CHECKING
+	#define REM_CHECK(x)
+#else
+	#define REM_CHECK(x) x
+#endif
 
 /*	Represents a number bound between a minium and maximum:
 		min <= value <= max
@@ -24,75 +32,78 @@
 		b += 10.0; // b = (1.0, 10.0, 10.0) Notice that value is capped at maximum (and is not 15.0 as it otherwise would be).
 		b == 10.0; // true (as value = 10.0)
 
-	Can be used with any type that supports arithmetic and comparisons, e.g. bound<int>, bound<float>, bound<MyCustomType>.
-*/
+	Can be used with any type that supports arithmetic and comparisons, e.g. bound<int>, bound<float>, bound<MyCustomType>. */
 template <class T>
 class bound {
 protected:
 	T min, val, max;
 
-	virtual void check() {
-		if (val > max) { val = max; }
-		else if (val < min) { val = min; }
-	}
-	void swap(bound& rhs) {
-		using std::swap;
-		swap(min, rhs.min);
-		swap(val, rhs.val);
-		swap(max, rhs.max);
+	friend void swap(bound& lhs, bound& rhs) noexcept(noexcept(T()))		// friend allows this to be a binary function (opposed to unary), and thus can be called instead of std::swap by STL containers
+	{
+		using std::swap;		
+		swap(lhs.min, rhs.min);
+		swap(lhs.val, rhs.val);
+		swap(lhs.max, rhs.max);
 	}
 
 public:
-	bound() : min(0), val(0), max(100) {}
-	bound(const T &val, const T &min, const T &max) : min(min), val(val), max(max) {}
-	bound(const bound& b) : min(b.min), val(b.val), max(b.max) {}
-	bound(bound&& b) : bound() { swap(b); }
+	bound() noexcept(noexcept(T())) : min(), val(), max()  {}
+	bound(const T &val, const T &min, const T &max) noexcept(noexcept(T())) : min(min), val(val), max(max) { REM_CHECK(check()); }
+	bound(const bound<T>& b) noexcept(noexcept(T())) : min(b.min), val(b.val), max(b.max) {}
+	bound(bound<T>&& b) noexcept(noexcept(T())) : bound() { swap(*this, b); }	// alternatively: bound(bound&& b) noexcept : bound(std::move(b.val), std::move(b.min), std::move(b.max)) { }
+	
 	~bound() {}
+
+	virtual void check()	// in order to offer no exceptions guarantee, need to guarantee > and < operators exist for type T.
+	{
+		if (val > max) { val = max; }
+		else if (val < min) { val = min; }
+	}
 
 	std::ostream& print(std::ostream& os) const { os << min << " <= " << val << " <= " << max; return os; }
 	
-	T range  () const { return max - min; }
-	T ratio	 () const { return (val - min) / (range()); }
+	T range() const { return max - min; }
+	T ratio() const { return (val - min) / range(); }
 
 	// Accessors
 
-	T& get    () { return val; }
-	T& get_min() { return min; }
-	T& get_max() { return max; }
+	T& get    () noexcept { return val; }
+	T& get_min() noexcept { return min; }
+	T& get_max() noexcept { return max; }
 
 	const T& get	() const { return val; }
 	const T& get_min() const { return min; }
 	const T& get_max() const { return max; }
 
-	void set    (const T &new_val) { val = new_val; check(); }
-	void set_min(const T &new_min) { min = new_min; check(); }
-	void set_max(const T &new_max) { max = new_max; check(); }
-	void set_all(const T &new_val, const T &new_min, const T &new_max) { val = new_val; min = new_min; max = new_max; check(); }
+	void set    (const T &new_val) { val = new_val; REM_CHECK(check()); }
+	void set_min(const T &new_min) { min = new_min; REM_CHECK(check()); }
+	void set_max(const T &new_max) { max = new_max; REM_CHECK(check()); }
+	void set_all(const T &new_val, const T &new_min, const T &new_max) { val = new_val; min = new_min; max = new_max; REM_CHECK(check()); }
 
 	// Type casts 
+	
+	int		to_int		() const { return (int		)val; }
+	long		to_long		() const { return (long		)val; }
+	float		to_float	() const { return (float	)val; }
+	double		to_double	() const { return (double	)val; }
+	long long	to_long_long	() const { return (long long	)val; }
+	std::string to_string   	() const { return std::to_string(val); }
 
-	int	to_int   () const { return (int   )val; }
-	float	to_float () const { return (float )val; }
-	double  to_double() const { return (double)val; }
-	std::string to_string() const { return std::to_string(val); }
-
-	operator int   () const { return (int   )val; }
-	operator float () const { return (float )val; }
-	operator double() const { return (double)val; }
+	operator T() const { return val; }
 
 	// Operator overloads 
 	
-	bound& operator  = (const T &rhs) { val  = rhs; check(); return *this; }
-	bound& operator += (const T &rhs) { val += rhs; check(); return *this; }
-	bound& operator -= (const T &rhs) { val -= rhs; check(); return *this; }
-	bound& operator *= (const T &rhs) { val *= rhs; check(); return *this; }
-	bound& operator /= (const T &rhs) { val /= rhs; check(); return *this; }
+	bound& operator  = (T rhs) {std::swap(val,rhs); REM_CHECK(check()); return *this; }   // copy-swap idiom
+	bound& operator += (const T &rhs) { val += rhs; REM_CHECK(check()); return *this; }
+	bound& operator -= (const T &rhs) { val -= rhs; REM_CHECK(check()); return *this; }
+	bound& operator *= (const T &rhs) { val *= rhs; REM_CHECK(check()); return *this; }
+	bound& operator /= (const T &rhs) { val /= rhs; REM_CHECK(check()); return *this; }
 
-	bound& operator  = (bound  rhs) { swap(rhs); return *this; } // copy-swap idiom
-	bound& operator += (const bound& rhs) { val += rhs.val; check(); return *this; }
-	bound& operator -= (const bound& rhs) { val -= rhs.val; check(); return *this; }
-	bound& operator *= (const bound& rhs) { val *= rhs.val; check(); return *this; }
-	bound& operator /= (const bound& rhs) { val /= rhs.val; check(); return *this; }
+	bound& operator  = (bound  rhs) noexcept(noexcept(T())) { swap(*this, rhs); return *this; } // copy-swap idiom
+	bound& operator += (const bound& rhs) { val += rhs.val; REM_CHECK(check()); return *this; }
+	bound& operator -= (const bound& rhs) { val -= rhs.val; REM_CHECK(check()); return *this; }
+	bound& operator *= (const bound& rhs) { val *= rhs.val; REM_CHECK(check()); return *this; }
+	bound& operator /= (const bound& rhs) { val /= rhs.val; REM_CHECK(check()); return *this; }
 
 	bool   operator == (const T &rhs) const { return     (val == rhs); }
 	bool   operator != (const T &rhs) const { return !operator==(rhs); }
@@ -109,11 +120,11 @@ public:
 	bool   operator >= (const bound& rhs) const { return !operator<(rhs); }
 
 	bound& operator++() {
-		++val; check();
+		++val; REM_CHECK(if (val > max) { val = max; });  // no need to check minium boundary
 		return *this;
 	}
 	bound& operator--() {
-		--val; check();
+		--val; REM_CHECK(if (val < min) { val = min; });	// not necessary to check maximum boundary
 		return *this;
 	}
 	const bound operator++(int unused) {
@@ -144,34 +155,12 @@ template <class T> bound<T> operator* (bound<T> lhs, const bound<T> & rhs) { lhs
 template <class T> bound<T> operator/ (bound<T> lhs, const bound<T> & rhs) { lhs /= rhs; return lhs; }
 
 template <class T> std::ostream& operator << (std::ostream& os, const bound<T>& a) { a.print(os); return os; }
-
-// expects input in the form of : val, min, max
 template <class T> std::istream& operator >> (std::istream& in, bound<T>& b) {
 	T x, y, z;
-
-	// read in value
-	in >> x;
-	b.set(x);
-
-	// skip delimiter
-	in.ignore(2);
-
-	// read minimum
-	in >> y;
-	b.set_min(y);
-
-	// skip delimiter
-	in.ignore(2);
-
-	// read in maximum
-	in >> z;
-	b.set_max(z);
-
+	in >> x; b.set(x);	// read val		:, min, max
+	in.ignore(2);		// skip delimiter	:min, max
+	in >> y; b.set_min(y);	// read minimum		:, max
+	in.ignore(2);		// skip delimiter	:max
+	in >> z; b.set_max(z);	// read maximum
 	return in;
-}
-
-namespace std {
-
-	template <class T> std::string to_string(const bound<T> &num) { return num.to_string();	}
-
 }
