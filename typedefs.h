913a896a24f9e9884811118818f877578ed82d84
Matt <m.p.law93@gmail.com>
2016-02-27 18:08:13 +0000

    Added files via upload

---
+++ typedefs.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// Standard type definitions for conciser code.
+//
+//
+//
+
+// string
+// ------------------------------------------------------------------------------------------------
+
+#include <string>
+
+typedef std::string str;
+typedef const std::string cstr;
+
+// int
+// ------------------------------------------------------------------------------------------------
+
+typedef const int cint;
+typedef unsigned int uint;
+typedef const unsigned int cuint;
